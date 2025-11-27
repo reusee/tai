8082422a499d205fd0cf32e3a1ed8ed851da155b
reus <reusee@gmail.com>
2025-11-27 17:21:48 +0800

    cmd/tai: refine error reporting

--- cmd/tai/main.go
+++ cmd/tai/main.go
@@ -3,17 +3,16 @@ package main
 import (
 	"os"
 
-	"github.com/reusee/e5"
 	"github.com/reusee/tai/tailang"
-)
-
-var (
-	ce = e5.Check.With(e5.WrapStacktrace)
 )
 
 func main() {
 	env := tailang.NewEnv()
 	tokenizer := tailang.NewTokenizer(os.Stdin)
 	_, err := env.Evaluate(tokenizer)
-	ce(err)
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+		os.Stderr.WriteString("\n")
+		os.Exit(-1)
+	}
 }
