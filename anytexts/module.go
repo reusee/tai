bde577e5f64ad0e6f27d98b9cd0e603eac36ca5b
reus <reusee@gmail.com>
2025-11-27 23:18:50 +0800

    anytexts: fix module

--- anytexts/module.go
+++ anytexts/module.go
@@ -3,9 +3,11 @@ package anytexts
 import (
 	"github.com/reusee/dscope"
 	"github.com/reusee/tai/generators"
+	"github.com/reusee/tai/phases"
 )
 
 type Module struct {
 	dscope.Module
 	Generators generators.Module
+	Phases     phases.Module
 }
