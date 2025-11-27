ff788ca18dde16e272b5285966c2ffcac81abe51
reus <reusee@gmail.com>
2025-11-27 08:22:00 +0800

    tailang: add Def.Type

--- tailang/def.go
+++ tailang/def.go
@@ -1,6 +1,13 @@
 package tailang
 
-type Def struct{}
+import (
+	"fmt"
+	"reflect"
+)
+
+type Def struct {
+	Type reflect.Type `tai:"type"`
+}
 
 var _ Function = Def{}
 
@@ -8,7 +15,24 @@ func (d Def) FunctionName() string {
 	return "def"
 }
 
-func (d Def) Call(env *Env, name string, value any) any {
+func (d Def) Call(env *Env, name string, value any) (any, error) {
+	if d.Type != nil {
+		if value == nil {
+			switch d.Type.Kind() {
+			case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Pointer, reflect.Slice:
+				// OK
+			default:
+				return nil, fmt.Errorf("cannot assign nil to %v", d.Type)
+			}
+		} else {
+			valV := reflect.ValueOf(value)
+			valV = convertType(valV, d.Type)
+			if !valV.Type().AssignableTo(d.Type) {
+				return nil, fmt.Errorf("cannot assign %v to %v", valV.Type(), d.Type)
+			}
+			value = valV.Interface()
+		}
+	}
 	env.Define(name, value)
-	return value
+	return value, nil
 }
--- tailang/def_test.go
+++ tailang/def_test.go
@@ -20,4 +20,28 @@ func TestDef(t *testing.T) {
 	if res != "42baz" {
 		t.Fatalf("got %v", res)
 	}
+
+	// typed
+	src = `
+		def .type int8 i 42
+		i
+	`
+	tokenizer = NewTokenizer(strings.NewReader(src))
+	res, err = env.Evaluate(tokenizer)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, ok := res.(int8); !ok {
+		t.Fatalf("expected int8, got %T", res)
+	}
+
+	// type mismatch
+	src = `
+		def .type int s "foo"
+	`
+	tokenizer = NewTokenizer(strings.NewReader(src))
+	_, err = env.Evaluate(tokenizer)
+	if err == nil {
+		t.Fatal("expected error")
+	}
 }
