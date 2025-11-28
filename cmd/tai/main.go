c24bb7c35a3cc9b2f02211c7f2abeb89f0ccde18
reus <reusee@gmail.com>
2025-11-28 16:42:32 +0800

    cmd/tai: refine argument

--- cmd/tai/main.go
+++ cmd/tai/main.go
@@ -8,7 +8,20 @@ import (
 
 func main() {
 	env := tailang.NewEnv()
-	tokenizer := tailang.NewTokenizer(os.Stdin)
+
+	var input = os.Stdin
+	if len(os.Args) > 1 {
+		f, err := os.Open(os.Args[1])
+		if err != nil {
+			os.Stderr.WriteString(err.Error())
+			os.Stderr.WriteString("\n")
+			os.Exit(-1)
+		}
+		defer f.Close()
+		input = f
+	}
+
+	tokenizer := tailang.NewTokenizer(input)
 	_, err := env.Evaluate(tokenizer)
 	if err != nil {
 		os.Stderr.WriteString(err.Error())
