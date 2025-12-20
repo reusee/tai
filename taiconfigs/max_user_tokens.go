82b0dd4b68ca0acb28142b9ee26c116df0f6be82
reus <reusee@gmail.com>
2025-12-20 16:01:34 +0800

    codes: add -max-user-tokens

---
+++ taiconfigs/max_user_tokens.go
@@ -0,0 +1,33 @@
+package taiconfigs
+
+import (
+	"math"
+
+	"github.com/reusee/tai/cmds"
+	"github.com/reusee/tai/configs"
+	"github.com/reusee/tai/vars"
+)
+
+type MaxUserTokens int
+
+var maxUserTokensFlag = cmds.Var[int]("-max-user-tokens")
+
+func (Module) MaxUserTokens(
+	loader configs.Loader,
+) MaxUserTokens {
+	maxTokens := math.MaxInt
+
+	// flag
+	if *maxUserTokensFlag != 0 {
+		maxTokens = min(maxTokens, *maxUserTokensFlag)
+	}
+
+	// config
+	if n := vars.FirstNonZero(
+		configs.First[int](loader, "max_user_tokens"),
+	); n != 0 {
+		maxTokens = min(maxTokens, n)
+	}
+
+	return MaxUserTokens(maxTokens)
+}
