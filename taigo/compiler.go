762b7af24651406f596f7a089b055d1973b52ceb
reus <reusee@gmail.com>
2025-12-24 14:17:03 +0800

    taigo: more compiler codes

--- taigo/compiler.go
+++ taigo/compiler.go
@@ -55,38 +55,7 @@ func (c *compiler) compileDecl(decl ast.Decl) error {
 	switch d := decl.(type) {
 
 	case *ast.FuncDecl:
-		sub := &compiler{
-			name: d.Name.Name,
-		}
-
-		for _, stmt := range d.Body.List {
-			if err := sub.compileStmt(stmt); err != nil {
-				return err
-			}
-		}
-
-		sub.loadConst(nil)
-		sub.emit(taivm.OpReturn)
-
-		fn := sub.getFunction()
-		if d.Type.Params != nil {
-			for _, field := range d.Type.Params.List {
-				if len(field.Names) == 0 {
-					fn.ParamNames = append(fn.ParamNames, "")
-				} else {
-					for _, name := range field.Names {
-						fn.ParamNames = append(fn.ParamNames, name.Name)
-					}
-				}
-			}
-		}
-		fn.NumParams = len(fn.ParamNames)
-
-		idx := c.addConst(fn)
-		c.emit(taivm.OpMakeClosure.With(idx))
-
-		nameIdx := c.addConst(d.Name.Name)
-		c.emit(taivm.OpDefVar.With(nameIdx))
+		return c.compileFuncDecl(d)
 
 	case *ast.GenDecl:
 		// TODO imports, const, type, var
@@ -99,57 +68,225 @@ func (c *compiler) compileDecl(decl ast.Decl) error {
 	return nil
 }
 
+func (c *compiler) compileFuncDecl(decl *ast.FuncDecl) error {
+	sub := &compiler{
+		name: decl.Name.Name,
+	}
+
+	for _, stmt := range decl.Body.List {
+		if err := sub.compileStmt(stmt); err != nil {
+			return err
+		}
+	}
+
+	sub.loadConst(nil)
+	sub.emit(taivm.OpReturn)
+
+	fn := sub.getFunction()
+	if decl.Type.Params != nil {
+		for _, field := range decl.Type.Params.List {
+			if len(field.Names) == 0 {
+				fn.ParamNames = append(fn.ParamNames, "")
+			} else {
+				for _, name := range field.Names {
+					fn.ParamNames = append(fn.ParamNames, name.Name)
+				}
+			}
+		}
+	}
+	fn.NumParams = len(fn.ParamNames)
+
+	idx := c.addConst(fn)
+	c.emit(taivm.OpMakeClosure.With(idx))
+
+	nameIdx := c.addConst(decl.Name.Name)
+	c.emit(taivm.OpDefVar.With(nameIdx))
+
+	return nil
+}
+
 func (c *compiler) compileExpr(expr ast.Expr) error {
 	switch e := expr.(type) {
 	case *ast.BasicLit:
-		switch e.Kind {
-		case token.INT:
-			v, err := strconv.ParseInt(e.Value, 0, 64)
-			if err != nil {
-				return err
-			}
-			c.loadConst(v)
-		case token.FLOAT:
-			v, err := strconv.ParseFloat(e.Value, 64)
-			if err != nil {
-				return err
-			}
-			c.loadConst(v)
-		case token.STRING:
-			v, err := strconv.Unquote(e.Value)
-			if err != nil {
-				return err
-			}
-			c.loadConst(v)
-		case token.CHAR:
-			v, _, _, err := strconv.UnquoteChar(e.Value, '\'')
-			if err != nil {
-				return err
-			}
-			c.loadConst(int64(v))
-		default:
-			return fmt.Errorf("unknown basic lit kind: %v", e.Kind)
-		}
+		return c.compileBasicLiteral(e)
 
 	case *ast.Ident:
-		switch e.Name {
-		case "true":
-			c.loadConst(true)
-		case "false":
-			c.loadConst(false)
-		case "nil":
-			c.loadConst(nil)
-		default:
-			idx := c.addConst(e.Name)
-			c.emit(taivm.OpLoadVar.With(idx))
-		}
+		return c.compileIdentifier(e)
+
+	case *ast.BinaryExpr:
+		return c.compileBinaryExpr(e)
+
+	case *ast.UnaryExpr:
+		return c.compileUnaryExpr(e)
+
+	case *ast.CallExpr:
+		return c.compileCallExpr(e)
 
 	case *ast.ParenExpr:
 		return c.compileExpr(e.X)
 
+	case *ast.IndexExpr:
+		if err := c.compileExpr(e.X); err != nil {
+			return err
+		}
+		if err := c.compileExpr(e.Index); err != nil {
+			return err
+		}
+		c.emit(taivm.OpGetIndex)
+
 	default:
 		return fmt.Errorf("unknown expr type: %T", expr)
 	}
+	return nil
+}
+
+func (c *compiler) compileBasicLiteral(expr *ast.BasicLit) error {
+	switch expr.Kind {
+
+	case token.INT:
+		v, err := strconv.ParseInt(expr.Value, 0, 64)
+		if err != nil {
+			return err
+		}
+		c.loadConst(v)
+
+	case token.FLOAT:
+		v, err := strconv.ParseFloat(expr.Value, 64)
+		if err != nil {
+			return err
+		}
+		c.loadConst(v)
+
+	case token.STRING:
+		v, err := strconv.Unquote(expr.Value)
+		if err != nil {
+			return err
+		}
+		c.loadConst(v)
+
+	case token.CHAR:
+		v, _, _, err := strconv.UnquoteChar(expr.Value, '\'')
+		if err != nil {
+			return err
+		}
+		c.loadConst(int64(v))
+
+	default:
+		return fmt.Errorf("unknown basic lit kind: %v", expr.Kind)
+	}
+
+	return nil
+}
+
+func (c *compiler) compileIdentifier(expr *ast.Ident) error {
+	switch expr.Name {
+	case "true":
+		c.loadConst(true)
+	case "false":
+		c.loadConst(false)
+	case "nil":
+		c.loadConst(nil)
+	default:
+		idx := c.addConst(expr.Name)
+		c.emit(taivm.OpLoadVar.With(idx))
+	}
+	return nil
+}
+
+func (c *compiler) compileBinaryExpr(expr *ast.BinaryExpr) error {
+	if err := c.compileExpr(expr.X); err != nil {
+		return err
+	}
+	if err := c.compileExpr(expr.Y); err != nil {
+		return err
+	}
+
+	switch expr.Op {
+	case token.ADD:
+		c.emit(taivm.OpAdd)
+	case token.SUB:
+		c.emit(taivm.OpSub)
+	case token.MUL:
+		c.emit(taivm.OpMul)
+	case token.QUO:
+		c.emit(taivm.OpDiv)
+	case token.REM:
+		c.emit(taivm.OpMod)
+	case token.EQL:
+		c.emit(taivm.OpEq)
+	case token.NEQ:
+		c.emit(taivm.OpNe)
+	case token.LSS:
+		c.emit(taivm.OpLt)
+	case token.LEQ:
+		c.emit(taivm.OpLe)
+	case token.GTR:
+		c.emit(taivm.OpGt)
+	case token.GEQ:
+		c.emit(taivm.OpGe)
+	case token.AND:
+		c.emit(taivm.OpBitAnd)
+	case token.OR:
+		c.emit(taivm.OpBitOr)
+	case token.XOR:
+		c.emit(taivm.OpBitXor)
+	case token.SHL:
+		c.emit(taivm.OpBitLsh)
+	case token.SHR:
+		c.emit(taivm.OpBitRsh)
+	default:
+		return fmt.Errorf("unknown binary operator: %s", expr.Op)
+	}
+
+	return nil
+}
+
+func (c *compiler) compileUnaryExpr(expr *ast.UnaryExpr) error {
+	switch expr.Op {
+
+	case token.SUB:
+		c.loadConst(0)
+		if err := c.compileExpr(expr.X); err != nil {
+			return err
+		}
+		c.emit(taivm.OpSub)
+
+	case token.ADD:
+		if err := c.compileExpr(expr.X); err != nil {
+			return err
+		}
+
+	default:
+		if err := c.compileExpr(expr.X); err != nil {
+			return err
+		}
+		switch expr.Op {
+
+		case token.NOT:
+			c.emit(taivm.OpNot)
+
+		case token.XOR:
+			c.emit(taivm.OpBitNot)
+
+		default:
+			return fmt.Errorf("unknown unary operator: %s", expr.Op)
+
+		}
+	}
+
+	return nil
+}
+
+func (c *compiler) compileCallExpr(expr *ast.CallExpr) error {
+	if err := c.compileExpr(expr.Fun); err != nil {
+		return err
+	}
+	for _, arg := range expr.Args {
+		if err := c.compileExpr(arg); err != nil {
+			return err
+		}
+	}
+	c.emit(taivm.OpCall.With(len(expr.Args)))
 	return nil
 }
 
@@ -188,6 +325,58 @@ func (c *compiler) compileStmt(stmt ast.Stmt) error {
 			c.emit(taivm.OpMakeTuple.With(len(s.Results)))
 			c.emit(taivm.OpReturn)
 		}
+
+	case *ast.AssignStmt:
+		if len(s.Lhs) != 1 || len(s.Rhs) != 1 {
+			return fmt.Errorf("only single assignment supported for now")
+		}
+
+		lhs := s.Lhs[0]
+		rhs := s.Rhs[0]
+
+		if idxExpr, ok := lhs.(*ast.IndexExpr); ok {
+			if err := c.compileExpr(idxExpr.X); err != nil {
+				return err
+			}
+			if err := c.compileExpr(idxExpr.Index); err != nil {
+				return err
+			}
+			if err := c.compileExpr(rhs); err != nil {
+				return err
+			}
+			c.emit(taivm.OpSetIndex)
+			return nil
+		}
+
+		if err := c.compileExpr(rhs); err != nil {
+			return err
+		}
+
+		if ident, ok := lhs.(*ast.Ident); ok {
+			idx := c.addConst(ident.Name)
+			if s.Tok == token.DEFINE {
+				c.emit(taivm.OpDefVar.With(idx))
+			} else {
+				c.emit(taivm.OpSetVar.With(idx))
+			}
+		} else {
+			return fmt.Errorf("assignment to %T not supported", lhs)
+		}
+
+	case *ast.IncDecStmt:
+		ident, ok := s.X.(*ast.Ident)
+		if !ok {
+			return fmt.Errorf("inc/dec only supported on identifiers")
+		}
+		idx := c.addConst(ident.Name)
+		c.emit(taivm.OpLoadVar.With(idx))
+		c.loadConst(1)
+		if s.Tok == token.INC {
+			c.emit(taivm.OpAdd)
+		} else {
+			c.emit(taivm.OpSub)
+		}
+		c.emit(taivm.OpSetVar.With(idx))
 
 	default:
 		return fmt.Errorf("unknown stmt type: %T", stmt)
---
+++ taigo/todo.go
@@ -0,0 +1,4 @@
+package taigo
+
+//TODO refine compileExpr to support all types
+//TODO refine compileStmt to support all types
