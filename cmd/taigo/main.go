406c931caae0ec403653426c3449c618c4fa50ab
reus <reusee@gmail.com>
2025-12-24 18:02:17 +0800

    add cmd/taigo

---
+++ cmd/taigo/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+
+	"github.com/reusee/tai/taigo"
+)
+
+func main() {
+
+	var input = os.Stdin
+	var name string
+	if len(os.Args) > 1 {
+		f, err := os.Open(os.Args[1])
+		if err != nil {
+			os.Stderr.WriteString(err.Error())
+			os.Stderr.WriteString("\n")
+			os.Exit(-1)
+		}
+		defer f.Close()
+		input = f
+		name = os.Args[1]
+	}
+
+	vm, err := taigo.NewVM(name, input)
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+		os.Stderr.WriteString("\n")
+		os.Exit(-1)
+	}
+
+	for _, err := range vm.Run {
+		if err != nil {
+			os.Stderr.WriteString(err.Error())
+			os.Stderr.WriteString("\n")
+			os.Exit(-1)
+		}
+	}
+
+}
