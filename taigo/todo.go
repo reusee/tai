23151fdf6f7915c384a5caf2f6545ddccea75240
reus <reusee@gmail.com>
2025-12-27 21:41:53 +0800

    taigo: update todo

--- taigo/todo.go
+++ taigo/todo.go
@@ -1,6 +1,19 @@
 package taigo
 
 /*
-intentional not supported:
+- recover
+- defer on panic
+- interface
+- auto take address on method calling
+- embedded field
+- iota
+- type assertion
+- fallthrough in switch
+- goto statement
+*/
+
+/*
+not supported intentionally:
 - modules and imports
+- concurrency: go statement, channels, select statement
 */
