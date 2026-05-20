5d360fae6bb84643f19a883c7e879a17795d9675
reus <reusee@gmail.com>
2026-05-20 17:02:25 +0800

    Revert "cmd/ai: Allow overriding the default generator with cmd_ai_model configuration"
    This reverts commit 420cf2c62b092e5c2f8f4a6c8e45bd4f0911aa89.

--- cmd/ai/generator.go
+++ cmd/ai/generator.go
@@ -1,29 +1,11 @@
 package main
 
-import (
-	"github.com/reusee/tai/configs"
-	"github.com/reusee/tai/generators"
-)
+import "github.com/reusee/tai/generators"
 
 func (Module) Generator(
 	getDefaultGenerator generators.GetDefaultGenerator,
-	getGenerator generators.GetGenerator,
-	commandModelName CommandModelName,
 ) generators.Generator {
-	if commandModelName != "" {
-		ret, err := getGenerator(string(commandModelName))
-		ce(err)
-		return ret
-	}
 	ret, err := getDefaultGenerator()
 	ce(err)
 	return ret
-}
-
-type CommandModelName string
-
-func (Module) CommandModelName(
-	loader configs.Loader,
-) CommandModelName {
-	return configs.First[CommandModelName](loader, "cmd_ai_model")
 }
