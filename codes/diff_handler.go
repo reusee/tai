0f5796166314e2b5ae67501b86d181b8d90638f7
reus <reusee@gmail.com>
2026-06-15 11:45:32 +0800

    Update XML diff handler prompts to enforce entire response be wrapped in an <xml> root element

--- codes/diff_handler.go
+++ codes/diff_handler.go
@@ -69,17 +69,22 @@ func (x XmlDiffHandler) Functions() []*generators.Function {
 }
 
 func (x XmlDiffHandler) SystemPrompt() string {
-	return `When you need to propose changes to files, you must output the changes in XML format.
-Each individual change must be a <change> element with the following attributes:
+	return `Your entire response must be a valid XML document with the root element <xml>.
+All content, including reasoning, explanations, and file changes, must be inside this <xml> root element.
+Do not output any text outside the <xml> document.
+You may include XML comments (<!-- ... -->) anywhere for reasoning and explanations.
+If no file changes are needed, output a minimal <xml></xml> or include a comment explaining why no changes are necessary.
+
+To propose changes to files, include <change> elements inside the <xml> root, with the following attributes:
 - op: one of MODIFY, ADD_BEFORE, ADD_AFTER, DELETE
 - target: the name of the declaration to modify, or BEGIN/END for file-level operations.
 - file-path: the path to the file to modify.
 
 The body of the <change> element must contain the new code exactly as it should appear.
 If the body contains characters that are special in XML (like <, >, &), you MUST enclose the entire body in a <![CDATA[ ... ]]> section.
-You may include XML comments (<!-- ... -->) outside the <change> elements for your reasoning.
 
 Example:
+<xml>
 <change op="MODIFY" target="Foo" file-path="test.go">
 <![CDATA[
 func Foo() {
@@ -87,10 +92,10 @@ func Foo() {
 }
 ]]>
 </change>
+</xml>
 `
 }
 
 func (x XmlDiffHandler) RestatePrompt() string {
-	return `Please output your file changes using the specified XML format.`
+	return `Please ensure your entire response is a valid XML document with the root element <xml>. Do not output any text outside the <xml> root.`
 }
-
