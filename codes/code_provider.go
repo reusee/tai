0d9511764c95ca70c8477c0835eefdcf1d9c5a5f
reus <reusee@gmail.com>
2026-07-29 13:34:17 +0800

    Simplify CodeProviderName to always return any and remove unused configurable variable

--- codes/code_provider.go
+++ codes/code_provider.go
@@ -4,18 +4,12 @@ import (
 	"github.com/reusee/tai/anytexts"
 	"github.com/reusee/tai/codes/codetypes"
 	"github.com/reusee/tai/gocodes"
-	"github.com/reusee/tai/vars"
 )
 
 type CodeProviderName string
 
-var codeProviderName CodeProviderName
-
 func (Module) CodeProviderName() CodeProviderName {
-	return vars.FirstNonZero(
-		codeProviderName,
-		"any",
-	)
+	return "any"
 }
 
 func (Module) CodeProvider(
